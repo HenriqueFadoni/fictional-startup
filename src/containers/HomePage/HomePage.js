b9c3700aa1c6232a585e98fca9538f51e200fafa
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-20 11:52:03 -0300

    Creating a Home Page

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import image1 from '../../images/image1.jpg';
+import image2 from '../../images/image2.jpg';
+import image3 from '../../images/image3.jpg';
+
+import './HomePage.scss';
+
+class HomePage extends Component {
+    state = {
+        image: [
+            image1,
+            image2,
+            image3
+        ],
+        number: 0
+    }
+
+    nextImage = () => {
+        let next = this.state.number;
+        if (next < 2) {
+            next += 1;
+            this.setState({number: next});
+        } else {
+            next = 0;
+            this.setState({number: next});
+        }
+    }
+
+    previousImage = () => {
+        let previous = this.state.number;
+        if (previous > 0) {
+            previous -= 1;
+            this.setState({number: previous});
+        } else {
+            previous = 2;
+            this.setState({number: previous});
+        }
+    }
+
+    render() {
+        const slideImage = this.state.image[this.state.number];
+
+        return (
+            <div className="homepage__container">
+                <h1>Tired of Living in Eath? <span>MarsTurism</span> is an option!</h1>
+                <div className="homepage__slider">
+                    <img src={slideImage} alt="Slide Show"/>
+                    <div className="btn__container">
+                        <button 
+                            className="btn btn-previous"
+                            onClick={this.previousImage}><ion-icon name="arrow-dropleft"></ion-icon></button>
+                        <button
+                            className="btn btn-next"
+                            onClick={this.nextImage}><ion-icon name="arrow-dropright"></ion-icon></button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
